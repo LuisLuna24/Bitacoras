bdc95f5faeafc90825554abd9d364ada6741430a
LuisLuna24 <eduarlun4@gmail.com>
2024-02-02 10:56:18 -0600

    Coneccion

--- php/conexion.php
+++ php/conexion.php
@@ -1,24 +1,24 @@
 <?php 
 try {
-    $host = 'localhost';
-    $user = 'postgres';
-    $password = 'Hmcnjsa1*.';
-    $dbname = 'pruebabitacoras';
+$host = 'localhost';
+$user = 'postgres';
+$password = 'Hmcnjsa1*.';
+$dbname = 'pruebabitacoras';
 
-    $conexion = pg_connect("host=$host dbname=$dbname user=$user password=$password");
-    if (!$conexion) {
-        die("Error al conectar a la base de datos");
-    }
+$conexion = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+if (!$conexion) {
+    die("Error al conectar a la base de datos");
+}
 }catch(ErrorException $e) {
-    $host = 'localhost';
-    $user = 'postgres';
-    $password = 'Integral3na2016';
-    $dbname = 'pruebabitacoras';
+$host = 'localhost';
+$user = 'postgres';
+$password = 'Integral3na2016';
+$dbname = 'pruebabitacoras';
 
-    $conexion = pg_connect("host=$host dbname=$dbname user=$user password=$password");
-    if (!$conexion) {
-        die("Error al conectar a la base de datos");
-    }
+$conexion = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+if (!$conexion) {
+    die("Error al conectar a la base de datos");
+}
 }
 
 ?>
