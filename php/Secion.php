951d6fdc041acdc792bfabf3144acd413c3f6c06
LuisLuna24 <eduarlun4@gmail.com>
2024-02-27 17:54:15 -0600

    Acceso a solo usuarios Activos

--- php/Secion.php
+++ php/Secion.php
@@ -12,7 +12,7 @@ $consulta=pg_query($conexion,$Buscar);
 if(pg_num_rows($consulta)!=0){
     $ContrasenaDes=$_POST['In_Contrasena'];
     $Contraseña=$encriptar($ContrasenaDes);
-    $BuscarUsu="SELECT * FROM usuario WHERE correo = '$Correo' and contrasena = '$Contraseña'";
+    $BuscarUsu="SELECT * FROM usuario WHERE correo = '$Correo' and contrasena = '$Contraseña' and estado_usuario='1'";
     $consulta=pg_query($conexion,$BuscarUsu);
     if(pg_num_rows($consulta)!=0){
         $row=pg_fetch_assoc($consulta);
